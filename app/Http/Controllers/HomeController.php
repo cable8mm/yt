3998101624e5e6607c0fde8bc07c7783ec970fd7
cable8mm <cable8mm@users.noreply.github.com>
2024-01-15 07:30:02 +0000

    coding style fixed (Pint)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     public function index()
     {
         $videos = Video::where('is_active', 1)->orderBy('published', 'desc')->paginate(18);
-      
+
         $feturedVideos = Video::where('is_active', 1)->where('is_live_broadcasting', 0)->orderBy('published', 'desc')->take(10)->get();
 
         return view('home', ['videos' => $videos, 'feturedVideos' => $feturedVideos]);
