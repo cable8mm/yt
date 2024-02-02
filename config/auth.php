aed2daeea225be84dee4846da11b1e7713e50b78
Sam Lee <cable8mm@gmail.com>
2024-02-03 01:09:11 +1300

    Create Admin model and renew ModelFactory

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'admins' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
     ],
 
     /*
@@ -69,6 +74,11 @@ return [
         //     'driver' => 'database',
         //     'table' => 'users',
         // ],
+
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
     ],
 
     /*
