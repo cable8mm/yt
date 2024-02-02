fcce6de918c1ed581177bc84623d77baa941bb18
Sam Lee <cable8mm@gmail.com>
2024-02-03 00:36:08 +1300

    Create migration files again

---
+++ database/migrations/2024_02_02_112040_create_imported_videos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imported_videos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('channel_id');
+            $table->string('service_clipid')->unique();
+            $table->string('kind');
+            $table->text('object');
+            $table->boolean('is_imported')->default(false);
+            $table->boolean('is_active')->default(true);
+            $table->dateTime('created')->nullable();
+            $table->dateTime('modified')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imported_videos');
+    }
+};
---
+++ database/migrations/2024_02_02_112040_create_videos_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('channel_id');
+            $table->string('videoId')->unique();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumbnail_url');
+            $table->string('medium_thumbnail_url');
+            $table->string('featured_image_url');
+            $table->text('embed_html');
+            $table->string('tag')->nullable();
+            $table->string('duration');
+            $table->string('license');
+            $table->boolean('has_caption')->default(false);
+            $table->boolean('is_live_broadcasting')->default(false);
+            $table->dateTime('scheduled_start_time')->nullable();
+            $table->dateTime('scheduled_end_time')->nullable();
+            $table->dateTime('published');
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_podcast_active')->default(false);
+            $table->dateTime('created')->nullable();
+            $table->dateTime('modified')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+};
