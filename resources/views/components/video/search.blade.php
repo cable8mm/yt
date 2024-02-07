8bbd86fac338d83de94c0a657314220b45857911
Sam Lee <cable8mm@gmail.com>
2024-02-08 01:59:22 +1300

    Replace q to query to fit laravel default query name and local scopes

--- resources/views/components/video/search.blade.php
+++ resources/views/components/video/search.blade.php
@@ -21,7 +21,7 @@
             </ul>
         </div>
         <div class="relative w-full">
-            <input type="search" id="search-dropdown" name="q" value="{{ $q }}"
+            <input type="search" id="search-dropdown" name="query" value="{{ $query }}"
                 class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
                 placeholder="Search video name..." required>
             <button type="submit"
