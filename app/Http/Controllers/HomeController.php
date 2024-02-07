c186a21371560adb25629b69c6593f1a874fe5bf
Sam Lee <cable8mm@gmail.com>
2024-02-07 14:57:51 +1300

    Remove some channels not to have any videos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,12 +11,14 @@ class HomeController extends Controller
     {
         $videos = Video::active()->ordered()->paginate(20);
         $feturedVideos = Video::active()->byLiveBroadCasting(false)->ordered()->take(10)->get();
-        $channels = Channel::active()->ordered()->get();
+        $ongoingChannels = Channel::withCount('videos')->where('videos_count', '>', 0)->active()->ordered()->get();
+        $futureChannels = Channel::withCount('videos')->where('videos_count', 0)->active();
 
         return view('home', [
             'videos' => $videos,
             'feturedVideos' => $feturedVideos,
-            'channels' => $channels,
+            'ongoingChannels' => $ongoingChannels,
+            'futureChannels' => $futureChannels,
         ]);
     }
 }
